7220a92b4b6d9ecbc0f1b0c4ac7d3e884de70d87
Dmitriy <dmitriy@ergonized.com>
2014-06-19 19:31:18 +0300

    fix resizer

--- imageResize.php
+++ imageResize.php
@@ -3,44 +3,11 @@ namespace vendors\imageresizer;
 
 class imageResize {
 
-    function __construct() {
-
-    }
-
-    public function resizeImgFile($source_path, $destination_path, $newheight = false, $newwidth = 300, $quality = false) {
-        ini_set("gd.jpeg_ignore_warning", 1);
-        list($oldwidth, $oldheight, $type) = getimagesize($source_path);
-        switch ($type) {
-            case IMAGETYPE_JPEG: $typestr = 'jpeg'; break;
-            case IMAGETYPE_GIF: $typestr = 'gif' ;break;
-            case IMAGETYPE_PNG: $typestr = 'png'; break;
-        }
-        $function = "imagecreatefrom$typestr";
-        $src_resource = $function($source_path);
-        unset($function);
-        $function = null;
-
-        if (!$newwidth) {
-            $newwidth = round($newheight * $oldwidth/$oldheight);
-        } elseif (!$newheight) {
-            $newheight = round($newwidth * $oldheight/$oldwidth);
-        }
-        $destination_resource = imagecreatetruecolor($newwidth, $newheight);
-
-        imagecopyresampled($destination_resource, $src_resource, 0, 0, 0, 0, $newwidth, $newheight, $oldwidth, $oldheight);
-
-        $function = "image$typestr";
-        $function($destination_resource, $destination_path);
-        unset($function);
-        $function = null;
-
-        imagedestroy($destination_resource);
-        unset($destination_resource);
-        $destination_resource = null;
-
-        imagedestroy($src_resource);
-        unset($src_resource);
-        $src_resource = null;
+    public function resizeImgFile($sourcePath, $destinationPath, $newHeight = false, $newWidth = 150) {
+        $image = new \Imagick($sourcePath);
+        $image->thumbnailImage($newWidth, $newHeight);
+        $image->writeImage($destinationPath);
+        unset($image);
     }
 
 }
