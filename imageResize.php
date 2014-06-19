c50d9843c22022385c0704941e4f7d2709f308b8
Dmitriy <dmitriy@ergonized.com>
2014-06-19 19:58:56 +0300

    cron_resize_images.php

--- imageResize.php
+++ imageResize.php
@@ -4,10 +4,14 @@ namespace vendors\imageresizer;
 class imageResize {
 
     public function resizeImgFile($sourcePath, $destinationPath, $newHeight = false, $newWidth = 150) {
-        $image = new \Imagick($sourcePath);
-        $image->thumbnailImage($newWidth, $newHeight);
-        $image->writeImage($destinationPath);
-        unset($image);
+        try {
+            $image = new \Imagick($sourcePath);
+            $image->thumbnailImage($newWidth, $newHeight);
+            $image->writeImage($destinationPath);
+            unset($image);
+        } catch (\Exception $e) {
+            echo 'Exception: ',  $e->getMessage(), "\n";
+        }
     }
 
 }
